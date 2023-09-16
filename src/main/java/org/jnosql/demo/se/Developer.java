12540f5d9f1b49440c8d0d7dd1944ab0aae42a3f
Maximillian Arruda <dearrudam@gmail.com>
2023-09-16 02:50:42 -0300

    docs: add open-source license
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- src/main/java/org/jnosql/demo/se/Developer.java
+++ src/main/java/org/jnosql/demo/se/Developer.java
@@ -1,3 +1,19 @@
+/*
+ *    Copyright 2023  Contributors to the Eclipse Foundation
+ *
+ *    All rights reserved. This program and the accompanying materials
+ *    are made available under the terms of the Eclipse Public License v1.0
+ *    and Apache License v2.0 which accompanies this distribution.
+ *    The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *    and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *    You may elect to redistribute this code under either of these licenses.
+ *
+ *    Contributors:
+ *
+ *    Maximillian Arruda
+ *
+ */
 package org.jnosql.demo.se;
 
 import jakarta.nosql.Column;
@@ -7,7 +23,7 @@ import jakarta.nosql.Id;
 import java.util.UUID;
 
 @Entity
-public record Developer(@Id String id,@Column String name) {
+public record Developer(@Id String id, @Column String name) {
     public static Developer of(String name) {
         return new Developer(UUID.randomUUID().toString(), name);
     }
